47cd971c640ad44c3b1defb99b9ec7a05df52489
Drarlian <witoredson@gmail.com>
2023-03-25 01:45:09 -0300

    Implementando os métodos responsáveis por guardar os dados dos clientes e das contas, além do método responsável por gerar um código unico para cada conta.

--- models/sistema.py
+++ models/sistema.py
@@ -1,4 +1,6 @@
 from typing import List
+from jsonpickle import encode, decode
+from os import path
 
 class Cliente:
     def __init__(self, nome: str, sobrenome: str, idade: int, cpf: int) -> None:
@@ -6,6 +8,7 @@ class Cliente:
         self.__sobrenome: str = sobrenome
         self.__idade: int = idade
         self.__cpf: int = cpf
+        self.__registrar_cliente()
 
     @property
     def nome(self) -> None:
@@ -26,12 +29,28 @@ class Cliente:
     def __str__(self) -> str:
         return f'Nome: {self.nome} | Sobrenome: {self.sobrenome} | Idade: {self.idade} | CPF: {self.cpf}'
 
+    def __registrar_cliente(self) -> None:
+        caminho = 'dados\\clientes.json'
+        if path.exists(caminho):
+            with open(caminho, 'r', encoding='UTF-8') as arquivo:
+                conteudo = arquivo.read()
+            with open(caminho, 'w', encoding='UTF-8') as arquivo:
+                if conteudo != '':
+                    ret: list = decode(conteudo)
+                    ret.append(self)
+                    ret = encode(ret)
+                    arquivo.write(ret)
+                else:
+                    ret = encode([self])
+                    arquivo.write(ret)
+
 
 class Conta:
     def __init__(self, cliente: Cliente) -> None:
         self.__cliente: Cliente = cliente
-        self.__numero_conta: int = None  # -> Implementar mais tarde a geração automatica do numero da conta.
+        self.__numero_conta: int = self.__gerar_numero_conta()
         self.__saldo: float = 0.0
+        self.__registrar_conta()
 
     @property
     def cliente(self) -> None:
@@ -46,7 +65,7 @@ class Conta:
         return self.__saldo
 
     def __str__(self) -> None:
-        return f'Cliente: {self.cliente} | Número da conta: {self.numero_conta} | Saldo: {self.saldo}'
+        return f'Cliente -> {self.cliente} | Conta-> Número da conta: {self.numero_conta} | Saldo: {self.saldo}'
 
     def sacar(self):
         pass
@@ -57,14 +76,65 @@ class Conta:
     def transferir(self):
         pass
 
+    def __gerar_numero_conta(self) -> int:
+        caminho = 'dados\\contas.json'
+        if path.exists(caminho):
+            with open(caminho, 'r', encoding='UTF-8') as arquivo:
+                conteudo = arquivo.read()
+                if conteudo != '':
+                    ret: list = decode(conteudo)
+                    codigo = ret[-1].numero_conta
+                    return codigo + 1
+                else:
+                    return 1
+        else:
+            return 1
+
+    def __registrar_conta(self) -> None:
+        caminho = 'dados\\contas.json'
+        if path.exists(caminho):
+            with open(caminho, 'r', encoding='UTF-8') as arquivo:
+                conteudo = arquivo.read()
+            with open(caminho, 'w', encoding='UTF-8') as arquivo:
+                if conteudo != '':
+                    ret: list = decode(conteudo)
+                    ret.append(self)
+                    ret = encode(ret)
+                    arquivo.write(ret)
+                else:
+                    ret = encode([self])
+                    arquivo.write(ret)
+
 
 class Banco:
     def __init__(self) -> None:
-        self.__clientes: List[Cliente] = self.__verifica_clientes()
-        self.__contas: List[Conta] = self.__verifica_contas()
+        self.__clientes: List[Cliente] = self.__buscar_clientes()
+        self.__contas: List[Conta] = self.__buscar_contas()
 
-    def __verifica_clientes(self):
-        pass
+    def imprimir_clientes(self) -> None:
+        for cliente in self.__clientes:
+            print(cliente)
 
-    def __verifica_contas(self):
-        pass
+    def imprimir_contas(self) -> None:
+        for conta in self.__contas:
+            print(conta)
+
+    def __buscar_clientes(self) -> list:
+        caminho = 'dados\\clientes.json'
+        if path.exists(caminho):
+            with open(caminho, 'r', encoding='UTF-8') as arquivo:
+                conteudo = arquivo.read()
+                ret: list = decode(conteudo)
+            return ret
+        else:
+            return None
+
+    def __buscar_contas(self) -> list:
+        caminho = 'dados\\contas.json'
+        if path.exists(caminho):
+            with open(caminho, 'r', encoding='UTF-8') as arquivo:
+                conteudo = arquivo.read()
+                ret: dict = decode(conteudo)
+            return ret
+        else:
+            return None
